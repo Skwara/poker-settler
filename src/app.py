3c6155ff8ee2d49522c8a8aa2dfa0fd9e45c89df
skwara <skwara0@gmail.com>
2023-03-27 23:25:42 +0200

    Add add-player flask route

--- src/app.py
+++ src/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return f"Hello from {app.name}"
+@app.route('/add-player/<name>/<float:buyin>/<float:cashout>')
+def add_player(name, buyin, cashout):
+    return f"NOT IMPLEMENTED: Added player {escape(name)} with buyin {buyin} cashed out with {cashout}"
 
 
 if __name__ == '__main__':
