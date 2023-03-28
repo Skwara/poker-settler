18de6399a52889b5637486596ee2af7100d7bba3
skwara <skwara0@gmail.com>
2023-03-28 18:40:46 +0200

    Add dummy home page with nav in base.html

--- src/app.py
+++ src/app.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
 @app.route('/add-player/<name>/<float:buyin>/<float:cashout>')
 def add_player(name, buyin, cashout):
     return render_template('add_player_result.html', name=name, buyin=buyin, cashout=cashout)
